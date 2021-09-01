38532c8afd9694a810657919188ce746de07861b
xeny <44981148+1x6@users.noreply.github.com>
2021-09-01 17:58:32 +0100

    igna ur so fussy

--- main.py
+++ main.py
@@ -8,21 +8,36 @@ username = input(f'{Fore.LIGHTMAGENTA_EX}Username: {Fore.RESET}')
 email = input(f'{Fore.LIGHTMAGENTA_EX}Email: {Fore.RESET}')
 password = input(f'{Fore.LIGHTMAGENTA_EX}Password: {Fore.RESET}')
 
-data = {
-    "birth_day":             "1",
-    "birth_month":           "01",
-    "birth_year":            "1970",
+resp = requests.post("https://spclient.wg.spotify.com/signup/public/v1/account", data={
+    "birth_day": "1",
+    "birth_month": "01",
+    "birth_year": "1970",
     "collect_personal_info": "undefined",
-    "creation_point":        "https://www.spotify.com/us/",
-    "displayname":           username,
-    "email":                 email,
-    "gender":                "male",
-    "iagree":                "1",
-	"key":                   "a1e486e2729f46d6bb368d6b2bcda326",
-	"password":              password,
-	"password_repeat":       password,
-	"username":              username,
-}
+    "creation_flow": "",
+    "creation_point": "https://www.spotify.com/uk/",
+    "displayname": username,
+    "username": username,
+    "gender": "neutral",
+    "iagree": "1",
+    "key": "a1e486e2729f46d6bb368d6b2bcda326",
+    "platform": "www",
+    "referrer": "https://www.spotify.com/uk/",
+    "send-email": "0",
+    "thirdpartyemail": "0",
+    "email": email,
+    "password": password,
+    "password_repeat": password
+}, headers={
+    "accept": "*/*",
+    "accept-language": "es-419,es;q=0.9",
+    "content-type": "application/x-www-form-urlencoded",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "sec-gpc": "1",
+    "referer": "https://www.spotify.com/",
+    "referrer-policy": "strict-origin-when-cross-origin"
+})
 
 resp = requests.post('https://spclient.wg.spotify.com/signup/public/v1/account', data=data)
 if "login_token" in resp.text:
